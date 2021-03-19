73f3792fdb1dfb759179efa61db193b3ab3302ab
zcong1993 <zhangcong1992@gmail.com>
2021-03-19 18:05:37 +0800

    bak

--- src/cache.ts
+++ src/cache.ts
@@ -2,13 +2,13 @@ import { createHash } from 'crypto'
 import { EventEmitter } from 'events'
 import { Singleflight } from '@zcong/singleflight'
 import { Redis } from 'ioredis'
-import { Codec, JSONCodec } from './codec'
+import { getCodec } from './codec'
 import IORedis = require('ioredis')
 
 export interface Option {
   redis: Redis
   prefix: string
-  codec?: Codec
+  codec?: string
 }
 
 export type Hasher = (...args: any[]) => string
@@ -22,17 +22,18 @@ export class RedisCache extends EventEmitter {
   constructor(private readonly option: Option) {
     super()
     if (!this.option.codec) {
-      this.option.codec = new JSONCodec()
+      this.option.codec = 'json'
     }
   }
 
   async cacheFn<T = any>(
     key: string,
     fn: () => Promise<T>,
-    expire: number
+    expire: number,
+    codec?: string
   ): Promise<T> {
     try {
-      const cached = await this.get(key)
+      const cached = await this.get(key, codec)
       if (cached !== null) {
         return cached
       }
@@ -47,7 +48,7 @@ export class RedisCache extends EventEmitter {
     return this.sf.do(key, async () => {
       const res = await fn()
       try {
-        await this.set(key, res, expire)
+        await this.set(key, res, expire, codec)
       } catch (err) {
         this.emit('error', {
           key,
@@ -63,24 +64,25 @@ export class RedisCache extends EventEmitter {
     keyPrefix: string,
     fn: T,
     expire: number,
+    codec?: string,
     keyHasher: Hasher = md5Hasher
   ): T {
     return (((...args: any[]) => {
       const cacheKey = RedisCache.joinKey(keyPrefix, keyHasher(...args))
-      return this.cacheFn(cacheKey, () => fn(...args), expire)
+      return this.cacheFn(cacheKey, () => fn(...args), expire, codec)
     }) as any) as T
   }
 
-  async get<T = any>(key: string): Promise<T> {
+  async get<T = any>(key: string, codec?: string): Promise<T> {
     const val = await this.option.redis.get(this.buildKey(key))
     if (!val) {
       return null
     }
-    return this.option.codec.decode(val)
+    return this.getCodecByName(codec).decode(val)
   }
 
-  async set<T = any>(key: string, val: T, expire: number) {
-    const vv = this.option.codec.encode(val)
+  async set<T = any>(key: string, val: T, expire: number, codec?: string) {
+    const vv = this.getCodecByName(codec).encode(val)
     await this.option.redis.set(this.buildKey(key), vv, 'ex', expire)
   }
 
@@ -90,6 +92,10 @@ export class RedisCache extends EventEmitter {
 
   private buildKey(key: string) {
     return RedisCache.joinKey(this.option.prefix, key)
+  }
+
+  private getCodecByName(codecName?: string) {
+    return getCodec(codecName ?? this.option.codec)
   }
 
   private static joinKey(...keys: string[]) {
@@ -129,10 +135,10 @@ const main = async () => {
 
   const fn = async (name: string, age: number) => {
     await sleep(1000)
-    return { name, age }
+    return `${name}:${age}`
   }
 
-  const fn2 = cc.cacheWrapper('fn', fn, 10)
+  const fn2 = cc.cacheWrapper('fn', fn, 10, 'raw')
 
   const res2 = await Promise.all(
     Array(10)
--- src/codec/store.ts
+++ src/codec/store.ts
@@ -1,23 +1,12 @@
 import { Codec } from './codec'
 import { JSONCodec } from './json'
+import { RawCodec } from './raw'
 
 let defaultCodec: Codec = new JSONCodec()
 
 let defaultSetted: boolean = false
 
 const codecMap = new Map<string, Codec>()
-
-export const getCodec = (name?: string) => {
-  if (!name) {
-    return defaultCodec
-  }
-
-  if (!codecMap.has(name)) {
-    throw new Error('codec not registered')
-  }
-
-  return codecMap.get(name)
-}
 
 export const registerCodec = (codec: Codec) => {
   codecMap.set(codec.name(), codec)
@@ -30,4 +19,20 @@ export const setDefaultCodec = (codec: Codec) => {
 
   defaultCodec = codec
   defaultSetted = true
+}
+
+registerCodec(new RawCodec())
+
+console.log(codecMap)
+
+export const getCodec = (name?: string) => {
+  if (!name) {
+    return defaultCodec
+  }
+
+  if (!codecMap.has(name)) {
+    throw new Error('codec not registered')
+  }
+
+  return codecMap.get(name)
 }
