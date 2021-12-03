aee8f28c89a17a54815c7e212e9437457f08c285
zcong1993 <zhangcong1992@gmail.com>
2021-12-03 11:45:21 +0800

    chore: use ts 4.5 Awaited

--- src/type.ts
+++ src/type.ts
@@ -1,16 +1,5 @@
-// copy from https://github.com/sindresorhus/type-fest avoid TS2742
-
-export type PromiseValue<
-  PromiseType,
-  Otherwise = PromiseType
-> = PromiseType extends Promise<infer Value>
-  ? { 0: PromiseValue<Value>; 1: Value }[PromiseType extends Promise<unknown>
-      ? 0
-      : 1]
-  : Otherwise
-
 type AsyncFunction = (...args: any[]) => Promise<unknown>
 
-export type AsyncReturnType<Target extends AsyncFunction> = PromiseValue<
+export type AsyncReturnType<Target extends AsyncFunction> = Awaited<
   ReturnType<Target>
 >
